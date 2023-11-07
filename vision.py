af4dd14805b01d6bffa2128cd5e2f107e0a0f6d2
Dan <dluttrell87@gmail.com>
2023-11-07 09:47:39 -0500

    Update vision.py
    added dalle3 parroting what vision saw

--- vision.py
+++ vision.py
@@ -2,18 +2,35 @@ import discord
 import os
 from openai import OpenAI
 
-openai.api_key = youropenaiapikey
+apikey = youropenaiapikey
 discord_apikey = yourdiscordapikey
 
+def getImage(description):
+    client = OpenAI(
+    api_key = apikey,
+    )
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=description,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    image_url = response.data[0].url
+    return image_url
+
 def getDescription(url):
-    client = OpenAI()
+    client = OpenAI(
+        api_key = apikey,
+    )
     response = client.chat.completions.create(
         model="gpt-4-vision-preview",
         messages=[
             {
                 "role": "user",
                 "content": [
-                    {"type": "text", "text": "What’s in this image?"},
+                    {"type": "text", "text": "Describe the image in detail using as many proper names for each of the items as possible."},
                     {
                         "type": "image_url",
                         "image_url": url,
@@ -36,5 +53,7 @@ async def on_message(message):
         for attachment in message.attachments:
             if attachment.filename.endswith('.jpg') or attachment.filename.endswith('.png'):
                 url = message.attachments[0].url
-                await message.reply(getDescription(url))
+                description = getDescription(url)
+                await message.reply(description + "\n\nHere's my best effort in creating the image:  " + getImage(description))
+
 client.run(yourdiscordapikey)
